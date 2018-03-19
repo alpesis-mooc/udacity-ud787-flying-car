f12953ba0b939c40b57e9a739dc88ae1c5b635da
K <kwailamchan@hotmail.com>
2018-03-20 01:14:07 +0800

    [master] integrated the codes. 	modified:   medial_axes.py

--- autopilot/graph/medial_axes.py
+++ autopilot/graph/medial_axes.py
@@ -9,21 +9,41 @@ from planning import a_star
 plt.rcParams["figure.figsize"] = [12, 12]
 
 
+def distance(x, y):
+    return np.sqrt((x[0] - y[0])**2 + (x[1] - y[1])**2)
+
+
+def heuristic(position, goal_position):
+    return np.abs(position[0] - goal_position[0]) + np.abs(position[1] - goal_position[1])
+
+
 def find_start_goal(skel, start, goal):
-    near_start = None
-    near_goal = None
-    for x in skel:
-        print(x)        
-        
+    skel_true = np.nonzero(skel)
+    skel_trans = np.transpose(skel_true)
+    distance_start_min = distance(skel_trans[0], start)
+    distance_goal_min = distance(skel_trans[0], goal)
+    near_start = (skel_trans[0][0], skel_trans[0][1])
+    near_goal = (skel_trans[0][0], skel_trans[0][1])
+    for x in skel_trans[1:]:
+        this_distance_start = distance(x, start)
+        this_distance_goal = distance(x, goal)
+        if this_distance_start < distance_start_min:
+            distance_start_min = this_distance_start
+            near_start = (x[0], x[1])
+        if this_distance_goal < distance_goal_min:
+            distance_goal_min = this_distance_goal
+            near_goal = (x[0], x[1])
     return near_start, near_goal
 
 
-def plot(grid, skeleton, start, goal):
+def plot(grid, skeleton, start, goal, skel_start, skel_goal):
     plt.imshow(grid, cmap="Greys", origin="lower")
     plt.imshow(skeleton, cmap="Greys", origin="lower", alpha=0.7)
 
     plt.plot(start[1], start[0], 'rx')
     plt.plot(goal[1], goal[0], 'rx')
+    plt.plot(skel_start[1], skel_start[0], 'x')
+    plt.plot(skel_goal[1], skel_goal[0], 'x')
 
     plt.xlabel('EAST')
     plt.ylabel('NORTH')
@@ -42,8 +62,8 @@ if __name__ == '__main__':
     safety_distance = 2
     grid = create_grid(data, drone_altitude, safety_distance)
     skeleton = medial_axis(invert(grid))
-    plot(grid, skeleton, start, goal)
 
     skel_start, skel_goal = find_start_goal(skeleton, start, goal)
     print(start, goal)
     print(skel_start, skel_goal)
+    plot(grid, skeleton, start, goal, skel_start, skel_goal)
